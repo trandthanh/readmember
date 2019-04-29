29b326c930cf12bbf8973060c78c1c2ea9cf45fd
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-04-29 20:57:00 +0200

    added title to notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_29_145313) do
+ActiveRecord::Schema.define(version: 2019_04_29_185622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_04_29_145313) do
     t.bigint "card_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["card_id"], name: "index_notes_on_card_id"
   end
 
